ce08ad5362e9c4900d8a444d2ba20c5c7fedb28a
Johan <Johan>
2017-06-26 13:26:25 +0200

    added .gitignore file extended the OandaSession class, so the accounts are handled properly
    Signed-off-by: Johan <kontakt@steunenberg.de>

--- simplev20/simplev20/oandasession.py
+++ simplev20/simplev20/oandasession.py
@@ -5,6 +5,8 @@ Created on Mon Jun 26 10:51:54 2017
 @author: Johan Steunenberg <kontakt@steunenberg.de>
 """
 
+from simplev20.oandaaccount import OandaAccount
+
 import os
 import v20
 
@@ -22,7 +24,30 @@ class OandaSession:
             self.url = "api-fxpractice.oanda.com"
         else:
             self.url = url
-        self.api = v20.Context(self.url, 443, token=self.key)
+        self.session = v20.Context(self.url, 443, token=self.key)
       
         self.accounts = {}
 
+        try:
+            accountlist = self.session.account.list().get('accounts')
+        except:
+            msg = 'One of the parameters key ({}) or url ({}) seems to be wrong'
+            raise ValueError(msg.format(self.key, self.url))
+            
+        for a in accountlist:
+            self.accounts[a.id] = OandaAccount(self.session, a.id)
+            acc = self.session.account.get(a.id)
+            acc = acc.get('account')
+            if acc.alias == 'Primary':
+                self.primary = self.accounts[a.id]
+ 
+    def get_accounts(self):
+        return self.accounts
+
+    def get_account(self, account_id):
+        return self.accounts[account_id]
+        
+    def get_primary_account(self):
+        return self.primary
+
+session = OandaSession()
--- simplev20/test/test-oandasession.py
+++ simplev20/test/test-oandasession.py
@@ -6,6 +6,7 @@ Created on Mon Jun 26 10:51:54 2017
 """
 
 from simplev20.oandasession import OandaSession
+from simplev20.oandaaccount import OandaAccount
 
 import unittest
 import os
@@ -25,6 +26,20 @@ class TestOandaSession(unittest.TestCase):
         """
         An empty test, asserting that the setup of the test works fine
         """
-        self.assertFalse(False)
+        self.assertIsNotNone(self.session)
+        
+    def test_init_loads_accounts(self):
+        self.assertIsNotNone(self.session)
+        self.assertGreater(len(self.session.accounts), 0)
+        self.assertIsNotNone(self.session.primary)
+        
+    def test_has_accounts(self):
+        self.assertIsNotNone(self.session.get_primary_account())
+        self.assertGreater(len(self.session.get_accounts()), 0)
+       
+    def test_get_account_from_id(self):
+        primary = self.session.get_primary_account()
+        test = self.session.get_account(primary.account_id)
+        self.assertEqual(primary, test)
 
 unittest.main()
