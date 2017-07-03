0d6ff10dda3b4b90277180e60b0a162b6466b3b7
Johan <Johan>
2017-07-03 21:42:32 +0200

    Created the order as a data container with two rw and two read-only attributes
    Signed-off-by: Johan <kontakt@steunenberg.de>

--- simplev20/simplev20/oandaorder.py
+++ simplev20/simplev20/oandaorder.py
@@ -7,8 +7,47 @@ Created on Mon Jul  3 20:12:57 2017
 
 class OandaOrder:
     """
-    This class encapsulates the values of an OandaOrder and
-    some means to manipulate them
+    This class encapsulates the values of an OandaOrder
+    For a start it's just a simple data container
+    using python properties    
     """
-    def __init__(self):
-        self.x = 'x'
+    def __init__(self, order_id, order_type, price, units):
+        self.__id = order_id
+        self.__type = order_type
+        self.price = price
+        self.units = units
+        
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, price):
+        if price <= 0:
+            raise ValueError('Negative prices are not possible')
+        else:
+            self.__price = price
+ 
+    @property
+    def units(self):
+        return self.__units
+ 
+    @units.setter
+    def units(self, units):
+        self.__units = units
+
+    """
+    type only has a getter
+    """
+    @property
+    def type(self):
+        return self.__type
+
+    """
+    id only has a getter
+    """
+    @property
+    def id(self):
+        return self.__id
+# 
+           
--- simplev20/test/test-oandaorder.py
+++ simplev20/test/test-oandaorder.py
@@ -6,3 +6,41 @@ Created on Mon Jul  3 20:12:57 2017
 """
 
 from simplev20.oandaorder import OandaOrder
+
+import unittest
+
+class TestOandaOrder(unittest.TestCase):
+    
+    def setUp(self):
+        self.order = OandaOrder(order_id='4567', 
+                                order_type='STOP', 
+                                price=1.23245, 
+                                units=10)
+
+    def test_create(self):
+        """
+        An simple test, asserting that the setup of the test
+        and the constructor of the account work fine
+        """
+        self.assertIsInstance(self.order, OandaOrder)
+        
+    def test_price_property(self):
+        self.assertEqual(self.order.price, 1.23245)
+        self.order.price += 0.001
+        self.assertEqual(self.order.price, 1.23345)
+        self.assertRaises(ValueError, setattr,  self.order, 'price', -1.)
+        
+    def test_units_property(self):
+        self.assertEqual(self.order.units, 10)
+        self.order.units += 10
+        self.assertEqual(self.order.units, 20)
+        
+    def test_type_property(self):
+        self.assertEqual(self.order.type, 'STOP')
+        self.assertRaises(AttributeError, setattr,  self.order, 'type', 'HURRAH')
+        
+    def test_id_property(self):
+        self.assertEqual(self.order.id, '4567')
+        self.assertRaises(AttributeError, setattr,  self.order, 'id', 4711)
+        
+unittest.main()        
